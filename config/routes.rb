bd225e4b3dfa99cd2b252d874a93657eb0e8b4e6
Marina Murashev <marinamurashev@gmail.com>
2014-01-06 09:32:19 -0600

    add user signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 SampleApp::Application.routes.draw do
   
+  get "users/new"
   root "static_pages#home"
 
+  get "/signup", to: "users#new"
   get "/help", to: "static_pages#help"
   get "/about", to: "static_pages#about"
   get "/contact", to: "static_pages#contact"
