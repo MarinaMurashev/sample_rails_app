b06eb34661ee00c25ce00bfd7fe95289975df0ed
Marina Murashev <marinamurashev@gmail.com>
2014-01-10 10:47:55 -0600

    add more tests to nonsignedin user view

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -16,7 +16,8 @@ describe "Authentication" do
 
     describe "with invalid information" do
       before { click_button "Sign in" }
-
+      it { should_not have_link("Profile") }
+      it { should_not have_link("Settings") }
       it { should have_error_message("Invalid") }
 
       describe "after visiting another page" do
