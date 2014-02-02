aac1a1cf84ed4ed7e367c5a2d40dc74e55b429cc
Marina Murashev <marinamurashev@gmail.com>
2014-02-02 14:46:16 -0600

    ensures microposts are returned in order, and that a user's microposts are deleted with its user

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@
 #
 
 class User < ActiveRecord::Base
-  has_many :microposts
+  has_many :microposts, dependent: :destroy
   has_secure_password
 
   before_save { self.email.downcase! }
@@ -21,7 +21,8 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, 
+    uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
 
